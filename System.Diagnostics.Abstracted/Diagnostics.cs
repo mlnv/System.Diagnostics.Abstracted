757b3693d80a2d60adbe771ab4e8c51ecd396d5f
Maksym Yemelianov <maxemev@gmail.com>
2022-03-22 19:12:27 +0100

    Remove .NET Framework 4.0 specific code

--- System.Diagnostics.Abstracted/Diagnostics.cs
+++ System.Diagnostics.Abstracted/Diagnostics.cs
@@ -5,21 +5,5 @@
         public IFileVersionInfoFactory FileVersionInfo => new FileVersionInfoFactory();
         public IStopwatchFactory Stopwatch => new StopwatchFactory();
         public IProcessFactory Process => new ProcessFactory();
-#if NET40
-        public IEventLog EventLog(string logName)
-        {
-            return new EventLog(logName);
-        }
-
-        public IEventLog EventLog(string logName, string machineName)
-        {
-            return new EventLog(logName, machineName);
-        }
-
-        public IEventLog EventLog(string logName, string machineName, string source)
-        {
-            return new EventLog(logName, machineName, source);
-        }
-#endif
     }
 }
